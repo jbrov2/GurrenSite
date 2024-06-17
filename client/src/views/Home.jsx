ee70e100d13c5a524072a0616d6d1e5395440aaa
jbrov2 <108750367+jbrov2@users.noreply.github.com>
2024-06-17 09:08:02 -0400

    intializing home page

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,7 +1,11 @@
 function Home() {
   return (
     <>
-      <div className=></div>
+      <div className='max-w-7x1 mx-auto px-4 md:px-6 lg:px-8 xl:px-1 h-screen bg-slate-900'>
+        <img className="object-cover w-full h-64 md:h-96" src="./img/Simon.png" alt="hero img"/>
+
+        
+      </div>
     </>
   );
 }
