9437ea2bee43be54b18a7fe715a18e770ba9f15e
zephyr <jafaribrownsha@gmail.com>
2024-06-19 15:00:59 -0400

    added font

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import './styles/index.css'
+// Supports weights 200-900
+import "@fontsource/rubik-mono-one";
+// Supports weights 300-700
+import "@fontsource-variable/comfortaa";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
