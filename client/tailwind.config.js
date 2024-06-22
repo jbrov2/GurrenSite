7d983661b2eebf23776f713205465ceb13be96f6
zephyr <jafaribrownsha@gmail.com>
2024-06-21 20:11:03 -0400

    test

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -28,12 +28,12 @@ export default {
       black: 900,
     },
     backgroundImage: {
-      hero: "url(./src/img/Simon.png)",
-      grid: "url(./src/img/grid.png)",
-      movie: "url(./src/img/gurrenMovie.jpg)",
-      mecha: "url(./src/img/mechs.jpg)",
-      shirts: "url(./src/img/Kamina Shirt.jpg)",
-      pops: "url(./src/img/pops.jpg)"
+      hero: "url(/src/img/Simon.png)",
+      grid: "url(/src/img/grid.png)",
+      movie: "url(/src/img/gurrenMovie.jpg)",
+      mecha: "url(/src/img/mechs.jpg)",
+      shirts: "url(/src/img/Kamina Shirt.jpg)",
+      pops: "url(/src/img/pops.jpg)",
     },
     extend: {
       colors: {
